66366c8a3f5728a6ed34c3a7c66a5330b4a59607
seddiq9721160210sepehr <seddiq.sepehr.9721160210@gmail.com>
2021-01-04 10:33:53 +0330

    1

--- 12/12.cpp
+++ 12/12.cpp
@@ -1,12 +1,13 @@
-#include<fstream>
 #include<iostream>
-
+#include<fstream>
 using namespace std;
 int main(){
     int i;
     ofstream f1("2.out.txt");
-    for(i=0 ;i < 10;i+2){
+    for(i=0 ;i < 10;i++){
+        if(i%2==0)
         f1 << i <<endl;
+
     }
     f1 << "sepehrseddiq" <<endl;
     f1.close();
